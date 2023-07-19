7bfb1434fffab6421e1bcd57f8db692b25a4ff68
Developer Avi <developer.aviral@gmail.com>
2023-07-19 12:42:56 +0530

    Import/Export Variables in Modules

---
+++ src/Module1.js
@@ -0,0 +1,8 @@
+import ui from './Module2.js'
+import {a,b,c,d} from './Module2.js'
+
+console.log(ui);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ src/Module2.js
@@ -0,0 +1,7 @@
+const a = "Aviral";
+const b = "Rahul";
+const c = "Priyanka"
+const d = "Anmol";
+
+export default a;
+export {a,b,c,d};
