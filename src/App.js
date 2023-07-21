346d7e81c9862a9404d99b4472242e23d5274772
Developer Avi <developer.aviral@gmail.com>
2023-07-21 14:12:21 +0530

    Adding BackTick Coding using usestate Hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 
+import { useState } from 'react';
 import './App.css';
-import About from './components/About.js';
+//import About from './components/About.js';
 import Navbar from './components/Navbar.js';
-//import TextForm from './components/TextForm.js';
+import TextForm from './components/TextForm.js';
 
 function App() {
+  const[mode, setMode] = useState('light');
+  const toggleMode= ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = "grey"
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = "white"
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutUs="About" />
+      <Navbar title="TextUtils" aboutUs="About" mode={mode} toggleMode={toggleMode}/>
       {/* <Navbar /> */}
       <div className='container'>
-        {/* <TextForm heading="Enter the text to analyze" /> */}
-        <About />
+        <TextForm heading="Enter the text to analyze" />
+        {/* <About /> */}
       </div>
     </>
 
